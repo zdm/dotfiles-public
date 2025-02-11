0648bf18f9b76fd83264963d6337766d8f359f9d
zdm <zdm@softvisio.net>
2025-02-11 17:25:45 +0200

    chore: update ssh

--- windows/ssh/install-sshd.cmd
+++ windows/ssh/install-sshd.cmd
@@ -5,7 +5,7 @@ call is-elevated.cmd || ( sudo -E "%~sf0" %* & exit /B )
 
 sc stop sshd
 sc delete sshd
-cygrunsrv -I sshd -d "sshd" -p /usr/bin/sshd.exe -a "-D -e" -y tcpip
 
 :: XXX not works
 :: sc create "sshd" binpath="c:\msys64\usr\bin\sshd.exe -D -e" start=auto
+cygrunsrv -I sshd -d "sshd" -p /usr/bin/sshd.exe -a "-D -e" -y tcpip
