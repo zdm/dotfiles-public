464b1d6362fc12a817ccd7a93c8ddd5e4c8d9daa
zdm <zdm@softvisio.net>
2025-02-11 13:17:52 +0200

    chore: update nvim

--- windows/install-software.cmd
+++ windows/install-software.cmd
@@ -3,9 +3,6 @@
 :: elevate script
 call is-elevated.cmd || ( sudo -E "%~sf0" %* & exit /B )
 
-:: winget install Neovim.Neovim
-:: winget install equalsraf.neovim-qt
-
 winget install Google.GoogleDrive
 winget install TeamViewer.TeamViewer.Host
 winget install VaclavSlavik.Poedit
