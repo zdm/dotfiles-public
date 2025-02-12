572f17541d1174c5b9414253ba755516de3ccfeb
zdm <zdm@softvisio.net>
2025-02-13 00:16:36 +0200

    chore: update ssh

--- windows/install-msys2.cmd
+++ windows/install-msys2.cmd
@@ -6,8 +6,8 @@ call is-elevated.cmd || ( sudo -E "%~sf0" %* & exit /B )
 setlocal
 
 set MSYS64_LOCATION="c:\msys64"
-set PROFILE="%MSYS64_LOCATION%\home\%USERNAME%"
-set GPG_PROFILE="s:\gpg"
+set MSYS64_USERPROFILE="%MSYS64_LOCATION%\home\%USERNAME%"
+set GPG_USERPROFILE="s:\gpg"
 
 winget install MSYS2.MSYS2
 
@@ -21,87 +21,63 @@ pacman -S --noconfirm --needed --refresh --sysupgrade
 :: tools
 pacman -S --noconfirm --needed bash pacman pacman-mirrors msys2-runtime diffutils man tar curl wget ctags whois patch mc
 
+:: bash
+del "%MSYS64_USERPROFILE%\.bashrc"
+mklink "%MSYS64_USERPROFILE%\.bashrc" "%~dp0\..\profile\.bashrc"
+
+del "%MSYS64_USERPROFILE%\.inputrc"
+mklink "%MSYS64_USERPROFILE%\.inputrc" "%~dp0\..\profile\.inputrc"
+
+:: mc
+mkdir "%MSYS64_USERPROFILE%\.config\mc"
+
+del "%MSYS64_USERPROFILE%\.config\mc\ini"
+mklink "%MSYS64_USERPROFILE%\.config\mc\ini" "%~dp0\..\profile\.config\mc\ini"
+
 :: git
 pacman -S --noconfirm --needed git git-filter-repo
 :: mingw-w64-x86_64-git-lfs
 
+del "%MSYS64_USERPROFILE%\.gitconfig"
+mklink "%MSYS64_USERPROFILE%\.gitconfig" "%~dp0\..\profile\.gitconfig"
+
+mkdir "%MSYS64_USERPROFILE%\.config\git"
+
+rmdir /S /Q "%MSYS64_USERPROFILE%\.config\git\hooks"
+mklink /D "%MSYS64_USERPROFILE%\.config\git\hooks" "%~dp0\..\profile\.config\git\hooks"
+
+rmdir /S /Q "%MSYS64_USERPROFILE%\.config\git\ssh"
+mklink /D "%MSYS64_USERPROFILE%\.config\git\ssh" "%~dp0\..\profile\.config\git\ssh"
+
 :: gpg
 pacman -S --noconfirm --needed gnupg
 
-:: ssh
-pacman -S --noconfirm --needed openssh cygrunsrv
+rmdir /S /Q "%MSYS64_USERPROFILE%\.gnupg"
+mklink /D "%MSYS64_USERPROFILE%\.gnupg" "%GPG_USERPROFILE%"
 
-:: remove unused packages
-pacman -Qqdt | pacman --noconfirm -Rns -
+del "%GPG_USERPROFILE%\dirmngr.conf"
+mklink "%GPG_USERPROFILE%\dirmngr.conf" "%~dp0\..\profile\.gnupg\dirmngr.conf"
 
-:: cleanup cache
-pacman --noconfirm -S --clean --clean
+del "%GPG_USERPROFILE%\gpg.conf"
+mklink "%GPG_USERPROFILE%\gpg.conf" "%~dp0\..\profile\.gnupg\gpg.conf"
 
-:: bash
-del "%PROFILE%\.bashrc"
-mklink "%PROFILE%\.bashrc" "%~dp0\..\profile\.bashrc"
+del "%GPG_USERPROFILE%\gpg-agent.conf"
+mklink "%GPG_USERPROFILE%\gpg-agent.conf" "%~dp0\..\profile\.gnupg\gpg-agent.conf"
 
-del "%PROFILE%\.inputrc"
-mklink "%PROFILE%\.inputrc" "%~dp0\..\profile\.inputrc"
-
-:: mc
-mkdir "%PROFILE%\.config\mc"
-
-del "%PROFILE%\.config\mc\ini"
-mklink "%PROFILE%\.config\mc\ini" "%~dp0\..\profile\.config\mc\ini"
-
-:: git
-del "%PROFILE%\.gitconfig"
-mklink "%PROFILE%\.gitconfig" "%~dp0\..\profile\.gitconfig"
-
-mkdir "%PROFILE%\.config\git"
-
-rmdir /S /Q "%PROFILE%\.config\git\hooks"
-mklink /D "%PROFILE%\.config\git\hooks" "%~dp0\..\profile\.config\git\hooks"
-
-rmdir /S /Q "%PROFILE%\.config\git\ssh"
-mklink /D "%PROFILE%\.config\git\ssh" "%~dp0\..\profile\.config\git\ssh"
-
-:: gpg
-rmdir /S /Q "%PROFILE%\.gnupg"
-mklink /D "%PROFILE%\.gnupg" "%GPG_PROFILE%"
-
-del "%GPG_PROFILE%\dirmngr.conf"
-mklink "%GPG_PROFILE%\dirmngr.conf" "%~dp0\..\profile\.gnupg\dirmngr.conf"
-
-del "%GPG_PROFILE%\gpg.conf"
-mklink "%GPG_PROFILE%\gpg.conf" "%~dp0\..\profile\.gnupg\gpg.conf"
-
-del "%GPG_PROFILE%\gpg-agent.conf"
-mklink "%GPG_PROFILE%\gpg-agent.conf" "%~dp0\..\profile\.gnupg\gpg-agent.conf"
-
-del "%GPG_PROFILE%\sshcontrol"
-mklink "%GPG_PROFILE%\sshcontrol" "%~dp0\..\profile\.gnupg\sshcontrol"
-
-FOR /F "delims=" %i IN ( 'gpgconf --list-dirs agent-ssh-socket' ) DO set SSH_AUTH_SOCK=%i
-setx /M SSH_AUTH_SOCK "%SSH_AUTH_SOCK%"
-
-:: generate sshd host keys
-ssh-keygen -A
-
-:: sshd
-mkdir ""%MSYS64_LOCATION%\etc\ssh"
-
-del "%MSYS64_LOCATION%\etc\ssh\ssh_config"
-mklink "%MSYS64_LOCATION%\etc\ssh\ssh_config" "%~dp0\ssh\ssh_config"
-
-del "%MSYS64_LOCATION%\etc\ssh\sshd_config"
-mklink "%MSYS64_LOCATION%\etc\ssh\sshd_config" "%~dp0\ssh\sshd_config"
+del "%GPG_USERPROFILE%\sshcontrol"
+mklink "%GPG_USERPROFILE%\sshcontrol" "%~dp0\..\profile\.gnupg\sshcontrol"
 
 :: ssh
-mkdir "%PROFILE%\.ssh"
+pacman -S --noconfirm --needed openssh
 
-del "%PROFILE%\.ssh\authorized_keys"
-mklink "%PROFILE%\.ssh\authorized_keys" "%~dp0\ssh\authorized_keys"
+mkdir "%MSYS64_USERPROFILE%\.ssh"
 
-del "%PROFILE%\.ssh\config"
-mklink "%PROFILE%\.ssh\config" "%~dp0\..\profile\.ssh\config"
+del "%MSYS64_USERPROFILE%\.ssh\config"
+mklink "%MSYS64_USERPROFILE%\.ssh\config" "%~dp0\..\profile\.ssh\config"
 
 setx /M LANGUAGE C.UTF-8
 setx /M LANG C.UTF-8
 setx /M LC_ALL C.UTF-8
+
+FOR /F "delims=" %i IN ( 'gpgconf --list-dirs agent-ssh-socket' ) DO set SSH_AUTH_SOCK=%i
+setx /M SSH_AUTH_SOCK "%SSH_AUTH_SOCK%"
