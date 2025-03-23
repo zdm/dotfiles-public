4400130a7d7d860b33d5af4c97820a43a70a089a
zdm <zdm@softvisio.net>
2025-03-24 01:13:57 +0200

    chore: update nvim

--- profile/.config/nvim/lua/plugins/tokyonight.lua
+++ profile/.config/nvim/lua/plugins/tokyonight.lua
@@ -1,11 +1,11 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  }}
+    {
+        "folke/tokyonight.nvim",
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        config = function ()
+            vim.cmd( [[colorscheme tokyonight]] )
+        end,
+    }
+}
