27e1c797a5690705d74c3afc44a05b8d75cea860
zdm <zdm@softvisio.net>
2025-03-24 11:48:55 +0200

    chore: update nvim

---
+++ profile/.config/nvim/lua/plugins/unite.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "shougo/unite.vim",
+        init = function ()
+            vim.g.unite_data_directory = vim.fn.stdpath( "data" ) .. "/unite"
+            vim.g.unite_source_bookmark_directory = vim.fn.stdpath( "config" ) .. "/unite/bookmark"
+            vim.g.unite_force_overwrite_statusline = 0
+            vim.g.unite_cursor_line_highlight = "CursorLine"
+            vim.g.unite_source_menu_menus = vim.empty_dict()
+            vim.g.unite_winheight = 20
+            vim.g.unite_winwidth = 40
+            vim.g.unite_split_rule = "botright"
+        end,
+    },
+    { "shougo/neomru.vim" },
+    { "shougo/unite-outline" },
+}
