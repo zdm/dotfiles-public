286122dca0de3ed3fea64d7c9390a994afaa432c
zdm <zdm@softvisio.net>
2025-03-24 11:39:06 +0200

    chore: update nvim

--- profile/.config/nvim/lua/plugins/init.lua
+++ profile/.config/nvim/lua/plugins/init.lua
@@ -73,60 +73,6 @@ return {
     { "vim-scripts/dirdiff.vim" },
     { "zhimsel/vim-stay" },
 
-    -- completion
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-vsnip",
-            "hrsh7th/vim-vsnip",
-            "hrsh7th/cmp-calc",
-        },
-        init = function ()
-            vim.o.omnifunc = "syntaxcomplete#Complete"
-            vim.o.completeopt = "menuone,noselect"
-            vim.opt.complete:remove( { "i" } )
-            vim.opt.iskeyword:append( { ":" } )
-
-            vim.g.vsnip_snippet_dir = vim.fn.stdpath( "config" ) .. "/vsnip"
-        end,
-        config = function ()
-            local cmp = require( "cmp" )
-
-            cmp.setup( {
-                snippet = {
-                    expand = function ( args )
-                        vim.fn[ "vsnip#anonymous" ]( args.body )
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert( {
-                    [ "<Up>" ] = function ( fallback )
-                        if cmp.visible() then
-                            cmp.abort()
-                        end
-
-                        fallback()
-                    end,
-                    [ "<Down>" ] = function ( fallback )
-                        if cmp.visible() then
-                            cmp.abort()
-                        end
-
-                        fallback()
-                    end,
-                    [ "<C-Up>" ] = cmp.mapping.select_prev_item( { behavior = cmp.SelectBehavior.Select } ),
-                    [ "<C-Down>" ] = cmp.mapping.select_next_item( { behavior = cmp.SelectBehavior.Select } ),
-                    [ "<C-Space>" ] = cmp.mapping.complete(),
-                    [ "<C-e>" ] = cmp.mapping.abort(),
-                    [ "<CR>" ] = cmp.mapping.confirm( { select = false } ),
-                } ),
-                sources = cmp.config.sources( {
-                    { name = "vsnip" },
-                    { name = "calc" },
-                } )
-            } )
-        end
-    },
-
     {
         "lyokha/vim-xkbswitch",
         dependencies = {
