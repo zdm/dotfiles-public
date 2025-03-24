2a8b031e9fd5fd2fcfd962f2f271ec3a0036d896
zdm <zdm@softvisio.net>
2025-03-24 11:43:02 +0200

    chore: update nvim

--- profile/.config/nvim/lua/plugins/init.lua
+++ profile/.config/nvim/lua/plugins/init.lua
@@ -23,51 +23,6 @@ return {
     { "vim-airline/vim-airline" },
     { "vim-airline/vim-airline-themes" },
 
-    -- comments
-    {
-        "numtostr/comment.nvim",
-        dependencies = {
-            "joosepalviste/nvim-ts-context-commentstring"
-        },
-        config = function ()
-            require( "ts_context_commentstring" ).setup( {
-                enable_autocmd = false,
-            } )
-
-            require( "Comment" ).setup( {
-                padding = true,
-                sticky = true,
-                ignore = "^$",
-                mappings = {
-                    basic = false,
-                    extra = false,
-                },
-                pre_hook = require( "ts_context_commentstring.integrations.comment_nvim" ).create_pre_hook(),
-            } )
-
-            local api = require( "Comment.api" )
-            local config = require( "Comment.config" ):get()
-
-            vim.keymap.set( { "n", "i" }, "<Leader>c", function ()
-                api.toggle.linewise.current()
-                vim.cmd( "normal! j" )
-            end )
-
-            vim.keymap.set( "v", "<Leader>c", api.call( "toggle.linewise", "g@" ), { expr = true } )
-            vim.keymap.set( "v", "<Leader>cc", api.call( "toggle.blockwise", "g@" ), { expr = true } )
-
-            local ft = require( "Comment.ft" )
-
-            ft.set( "dosbatch", ":: %s" )
-
-            -- XXX
-            -- let g:tcomment#replacements_xml = {
-            -- \   '<!--': '<!-&#45;',
-            -- \   '-->': '&#45;->'
-            -- \ }
-        end,
-    },
-
     { "tpope/vim-fugitive" },
     { "uguu-org/vim-matrix-screensaver" },
     { "vim-scripts/dirdiff.vim" },
