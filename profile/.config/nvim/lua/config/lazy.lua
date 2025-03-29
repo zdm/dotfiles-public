bda8642343568a73752e6637f41c2b164faf1c7d
zdm <zdm@softvisio.net>
2025-03-29 03:02:01 +0200

    chore: update nvim

--- profile/.config/nvim/lua/config/lazy.lua
+++ profile/.config/nvim/lua/config/lazy.lua
@@ -19,23 +19,34 @@ end
 
 vim.opt.rtp:prepend( lazypath )
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
 require( "lazy" ).setup( {
+    lockfile = vim.fn.stdpath( "data" ) .. "/lazy/lazy-lock.json",
     spec = {
-        -- import your plugins
-        { import = "plugins" },
+        {
+            import = "plugins"
+        },
     },
-
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
-    -- install = { colorscheme = { "habamax" } },
-
-    -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = {
+        enabled = true
+    },
+    ui = {
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
 } )
