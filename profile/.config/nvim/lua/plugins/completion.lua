2dcbd37c428710d7cc1039abcdcdb875b633fa26
zdm <zdm@softvisio.net>
2025-03-31 08:14:02 +0300

    chore: update nvim

--- profile/.config/nvim/lua/plugins/completion.lua
+++ profile/.config/nvim/lua/plugins/completion.lua
@@ -11,8 +11,9 @@ return {
                     vim.g.vsnip_snippet_dir = vim.fn.stdpath( "config" ) .. "/vsnip"
 
                     vim.g.vsnip_filetypes = {
+                        [ "html/javascript" ] = { "javascript", "html/javascript" },
                         [ "vue" ] = { "html", "vue/html" },
-                        [ "vue/javascript" ] = { "javascript", "vue/javascript" },
+                        [ "vue/javascript" ] = { "javascript", "html/javascript", "vue/javascript" },
                     }
                 end
             },
