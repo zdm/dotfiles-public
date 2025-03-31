ad5d60b6c2d71c4fe14fa5b3cee83c022a2da8fd
zdm <zdm@softvisio.net>
2025-03-31 12:51:40 +0300

    chore: update nvim

--- profile/.config/nvim/queries/vim/folds.scm
+++ profile/.config/nvim/queries/vim/folds.scm
@@ -1,8 +1,9 @@
 [
-    (function_definition)
-    (lua_statement)
-    (ruby_statement)
-    (python_statement)
-    (perl_statement)
-    (autocmd_statement)
+  ; (if_statement)
+  (function_definition)
+  (lua_statement)
+  (ruby_statement)
+  (python_statement)
+  (perl_statement)
+  (autocmd_statement)
 ] @fold
