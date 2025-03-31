950a8c640f019d57d1ac576628e6e31cfc538769
zdm <zdm@softvisio.net>
2025-03-31 14:42:39 +0300

    chore: update nvim

--- profile/.config/nvim/lua/config/lazy.lua
+++ profile/.config/nvim/lua/config/lazy.lua
@@ -32,7 +32,11 @@ require( "lazy" ).setup( {
         },
     },
     checker = {
-        enabled = true
+        enabled = false
+    },
+    change_detection = {
+        enabled = true,
+        notify = false,
     },
     ui = {
         icons = {
