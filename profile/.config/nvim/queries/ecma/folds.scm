5c44c6fe4e77cda306bac6f590bdbd0bdc72cb77
zdm <zdm@softvisio.net>
2025-03-31 12:42:13 +0300

    chore: update nvim

--- profile/.config/nvim/queries/ecma/folds.scm
+++ profile/.config/nvim/queries/ecma/folds.scm
@@ -2,6 +2,7 @@
     (arrow_function)
     (function_expression)
     (function_declaration)
+    (class_declaration)
     (method_definition)
     (generator_function)
     (generator_function_declaration)
