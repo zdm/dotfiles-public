d07cbe18f043ee3c5a1e1426043304db2f9ffe06
zdm <zdm@softvisio.net>
2025-04-02 19:18:41 +0300

    chore: update nvim

--- profile/.config/nvim/lua/plugins/other.lua
+++ profile/.config/nvim/lua/plugins/other.lua
@@ -82,20 +82,20 @@ return {
         config = function ()
             require( "gitsigns" ).setup( {
                 signs = {
-                    add          = { text = '┃' },
-                    change       = { text = '┃' },
-                    delete       = { text = '_' },
-                    topdelete    = { text = '‾' },
-                    changedelete = { text = '~' },
-                    untracked    = { text = '┆' },
+                    add          = { text = "┃" },
+                    change       = { text = "┃" },
+                    delete       = { text = "_" },
+                    topdelete    = { text = "‾" },
+                    changedelete = { text = "~" },
+                    untracked    = { text = "┆" },
                 },
                 signs_staged = {
-                    add          = { text = '┃' },
-                    change       = { text = '┃' },
-                    delete       = { text = '_' },
-                    topdelete    = { text = '‾' },
-                    changedelete = { text = '~' },
-                    untracked    = { text = '┆' },
+                    add          = { text = "┃" },
+                    change       = { text = "┃" },
+                    delete       = { text = "_" },
+                    topdelete    = { text = "‾" },
+                    changedelete = { text = "~" },
+                    untracked    = { text = "┆" },
                 },
                 signs_staged_enable = true,
                 signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
@@ -110,26 +110,26 @@ return {
                 current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
                 current_line_blame_opts = {
                     virt_text = true,
-                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    virt_text_pos = "eol", -- "eol", "overlay", "right_align"
                     delay = 1000,
                     ignore_whitespace = false,
                     virt_text_priority = 100,
                     use_focus = true,
                 },
-                current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+                current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
                 sign_priority = 6,
                 update_debounce = 100,
                 status_formatter = nil, -- Use default
                 max_file_length = 40000, -- Disable if file is longer than this (in lines)
                 preview_config = {
                     -- Options passed to nvim_open_win
-                    border = 'single',
-                    style = 'minimal',
-                    relative = 'cursor',
+                    border = "single",
+                    style = "minimal",
+                    relative = "cursor",
                     row = 0,
                     col = 1
                 },
-                debug_mode = true,
+                debug_mode = false,
             } )
         end,
     }
