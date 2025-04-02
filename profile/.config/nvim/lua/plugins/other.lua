aa54d423ee8180ba098b19762f56195448136e85
zdm <zdm@softvisio.net>
2025-04-02 18:12:08 +0300

    chore: update nvim

--- profile/.config/nvim/lua/plugins/other.lua
+++ profile/.config/nvim/lua/plugins/other.lua
@@ -28,13 +28,6 @@ return {
         end
     },
     {
-        "mhinz/vim-signify",
-        init = function ()
-            vim.g.signify_vcs_list = { "git" }
-            vim.g.signify_realtime = 1
-        end,
-    },
-    {
         "yggdroot/indentline",
         init = function ()
             vim.g.indentLine_enabled = 1
@@ -50,7 +43,6 @@ return {
             end, { noremap = true, silent = true } )
         end
     },
-    { "tpope/vim-fugitive" },
     { "uguu-org/vim-matrix-screensaver" },
     { "vim-scripts/dirdiff.vim" },
     { "zhimsel/vim-stay" },
@@ -75,4 +67,70 @@ return {
             end
         end
     },
+
+    -- git
+    -- { "tpope/vim-fugitive" },
+    -- {
+    --     "mhinz/vim-signify",
+    --     init = function ()
+    --         vim.g.signify_vcs_list = { "git" }
+    --         vim.g.signify_realtime = 1
+    --     end,
+    -- },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function ()
+            require( "gitsigns" ).setup( {
+                signs = {
+                    add          = { text = '┃' },
+                    change       = { text = '┃' },
+                    delete       = { text = '_' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
+                },
+                signs_staged = {
+                    add          = { text = '┃' },
+                    change       = { text = '┃' },
+                    delete       = { text = '_' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
+                },
+                signs_staged_enable = true,
+                signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+                numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+                linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
+                word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
+                watch_gitdir = {
+                    follow_files = true
+                },
+                auto_attach = true,
+                attach_to_untracked = false,
+                current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    delay = 1000,
+                    ignore_whitespace = false,
+                    virt_text_priority = 100,
+                    use_focus = true,
+                },
+                current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+                sign_priority = 6,
+                update_debounce = 100,
+                status_formatter = nil, -- Use default
+                max_file_length = 40000, -- Disable if file is longer than this (in lines)
+                preview_config = {
+                    -- Options passed to nvim_open_win
+                    border = 'single',
+                    style = 'minimal',
+                    relative = 'cursor',
+                    row = 0,
+                    col = 1
+                },
+                debug_mode = true,
+            } )
+        end,
+    }
 }
