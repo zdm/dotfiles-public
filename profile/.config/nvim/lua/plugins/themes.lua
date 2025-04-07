d0eff2d202cf82b47fc906ae10bbd2739460fce1
zdm <zdm@softvisio.net>
2025-04-08 00:01:59 +0300

    chore: update nvim

--- profile/.config/nvim/lua/plugins/themes.lua
+++ profile/.config/nvim/lua/plugins/themes.lua
@@ -116,7 +116,7 @@ return {
             vim.g.srcery_inverse = 0
             vim.g.srcery_inverse_matches = 0
             vim.g.srcery_inverse_match_paren = 0
-            vim.g.srcery_bg = { vim.g.srcery_black, 0 }
+            vim.g.srcery_bg = { vim.g.srcery_black, vim.g.srcery_black }
         end,
         config = function ()
             vim.cmd.colorscheme( "srcery" )
