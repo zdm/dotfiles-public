f6739cf90596b9090afce96b708ffc9716f3fc69
zdm <zdm@softvisio.net>
2025-04-08 13:44:08 +0300

    chore: update nvim

--- profile/.config/nvim/lua/lualine/components/progress.lua
+++ profile/.config/nvim/lua/lualine/components/progress.lua
@@ -2,13 +2,7 @@ local function progress ()
     local total_lines = vim.fn.line( "$" )
     local line = vim.fn.line( "." )
 
-    if line == 1 then
-        return "  1%%"
-    elseif line == total_lines then
-        return "100%%"
-    else
-        return string.format( " %2d%%%%", math.floor( line / total_lines * 100 ) )
-    end
+    return string.format( " %2d%%%%", math.floor( line / total_lines * 100 ) )
 end
 
 return progress
