759affbdaee02c3f2c831676a3c837220e1030d2
zdm <zdm@softvisio.net>
2025-04-08 17:35:44 +0300

    chore: update nvim

--- profile/.config/nvim/lua/lualine/components/location.lua
+++ profile/.config/nvim/lua/lualine/components/location.lua
@@ -3,7 +3,7 @@ local function location ()
     local line = vim.fn.line( "." )
     local column = vim.fn.charcol( "." )
 
-    return string.format( "line: %d/%d  col:%d", line, total_lines, column )
+    return string.format( "line: %d/%d  col: %d", line, total_lines, column )
 end
 
 return location
