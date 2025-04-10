9074af52890ebda444264141b7062cc686074c66
zdm <zdm@softvisio.net>
2025-04-10 08:30:11 +0300

    chore: update nvim

--- profile/.config/nvim/lua/plugins/statuscolumn.lua
+++ profile/.config/nvim/lua/plugins/statuscolumn.lua
@@ -1,31 +1,34 @@
 local folds = function ( options )
-    local foldlevel = vim.fn.foldlevel( vim.v.lnum )
-    local foldlevel_before = vim.fn.foldlevel( ( vim.v.lnum - 1 ) >= 1 and vim.v.lnum - 1 or 1 )
-    local foldlevel_after = vim.fn.foldlevel( ( vim.v.lnum + 1 ) <= vim.fn.line( "$" ) and ( vim.v.lnum + 1 ) or vim.fn.line( "$" ) )
-    local foldclosed = vim.fn.foldclosed( vim.v.lnum )
+    local foldlevel = vim.fn.foldlevel( options.lnum )
 
-    -- Line has nothing to do with folds so we will skip it
+    -- not a fold
     if foldlevel == 0 then
         return " "
     end
 
-    -- Line is a closed fold(I know second condition feels unnecessary but I will still add it)
-    if foldclosed ~= -1 and foldclosed == vim.v.lnum then
+    local last_line = vim.fn.line( "$" )
+    local foldlevel_before = vim.fn.foldlevel( ( options.lnum - 1 ) >= 1 and options.lnum - 1 or 1 )
+    local foldlevel_after = vim.fn.foldlevel( ( options.lnum + 1 ) <= last_line and ( options.lnum + 1 ) or last_line )
+    local foldclosed = vim.fn.foldclosed( options.lnum )
+
+    -- close fold
+    if foldclosed ~= -1 and foldclosed == options.lnum then
         return options.fold.close
-    end
 
-    -- I didn't use ~= because it couldn't make a nested fold have a lower level than it's parent fold and it's not something I would use
-    if foldlevel > foldlevel_before then
+    -- open fold
+    elseif foldlevel > foldlevel_before then
         return options.fold.open
-    end
 
-    -- The line is the last line in the fold
-    if foldlevel > foldlevel_after then
-        return "╰" -- "└╰++＋﹢" -- "└" -- "╰"
-    end
+    -- end fold
+    elseif foldlevel > foldlevel_after or options.lnum == last_line  then
+        -- return "╰"
+        return "└" -- "╰++＋﹢" -- "└" -- "╰"
 
-    -- Line is in the middle of an open fold
-    return "╎" -- options.fold.sep -- "╎"
+    -- inside fold
+    else
+       return options.fold.sep
+       -- return "╎"
+    end
 end
 
 return {
