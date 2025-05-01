db3e40a4d9a304b3d1883e2c3683ffe9cf5d3844
zdm <zdm@softvisio.net>
2025-05-01 09:31:43 +0300

    chore: update git

--- profile/.config/nvim/lua/plugins/diff.lua
+++ profile/.config/nvim/lua/plugins/diff.lua
@@ -2,9 +2,11 @@ return {
     {
         "sindrets/diffview.nvim",
         config = function ()
-            -- local actions = require( "diffview.actions" )
+            local actions = require( "diffview.actions" )
 
-            -- require( "diffview" ).setup( {} )
+            require( "diffview" ).setup( {
+                 git_cmd = { vim.fn.has( "win32" ) == 1 and "git.exe" or "git" },
+            } )
         end
     },
 }
