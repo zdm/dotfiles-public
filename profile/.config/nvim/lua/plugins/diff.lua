5983cc69126fb512423e839ecda06f96808e4e6c
zdm <zdm@softvisio.net>
2025-05-01 20:38:16 +0300

    chore: update git

--- profile/.config/nvim/lua/plugins/diff.lua
+++ profile/.config/nvim/lua/plugins/diff.lua
@@ -8,6 +8,25 @@ return {
             require( "diffview" ).setup( {
                 git_cmd = { vim.fn.has( "win32" ) == 1 and "git.exe" or "git" },
                 use_icons = false,
+                view = {
+                    merge_tool = {
+                        layout = "diff3_mixed", -- "diff1_plain"
+                        disable_diagnostics = true,
+                        winbar_info = true,
+                    },
+                },
+                file_panel = {
+                    listing_style = "list", -- "tree"
+                    tree_options = {
+                        flatten_dirs = true,
+                        folder_statuses = "only_folded",  -- "never", "only_folded", "always"
+                    },
+                    win_config = {
+                        position = "left",
+                        width = 30,
+                        win_opts = {},
+                    },
+                },
             } )
         end
     },
