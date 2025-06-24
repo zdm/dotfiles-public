405fc2e732459316f579f27a8b443bf957f1f914
zdm <zdm@softvisio.net>
2025-06-24 06:07:44 +0300

    refactor: update dotfiles

--- after-update.sh
+++ after-update.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-source $DOTFILES_DESTINATION/.bashrc
+function _after-update() {
+    source $DOTFILES_DESTINATION/.bashrc
 
-# postgresql
-# mkdir -p /etc/postgresql-common
-# \cp $DOTFILES_SOURCE/.psqlrc /etc/postgresql-common/psqlrc
+    # postgresql
+    # mkdir -p /etc/postgresql-common
+    # \cp $DOTFILES_SOURCE/.psqlrc /etc/postgresql-common/psqlrc
+}
+
+_after-update
