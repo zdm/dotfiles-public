612a47b3bb1105d54a23591e8f2ea134794b3a37
zdm <zdm@softvisio.net>
2025-10-15 06:51:39 +0300

    chore: update winget

--- windows/install-software.cmd
+++ windows/install-software.cmd
@@ -5,6 +5,7 @@ call is-elevated.cmd || ( sudo -E "%~sf0" %* & exit /B )
 
 winget install ^
     Google.GoogleDrive ^
+    Google.GoogleUpdater ^
     TeamViewer.TeamViewer.Host ^
     VaclavSlavik.Poedit ^
     Garmin.Express ^
