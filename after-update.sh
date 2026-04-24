6c5535d0a9c095c24db4835111bf434b83429ca1
zdm <zdm@softvisio.net>
2026-04-24 22:55:33 +0300

    refactor: update sh error trap

--- after-update.sh
+++ after-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
+trap 'echo "⚠  Error ($0:$LINENO): $BASH_COMMAND" && return 3 2> /dev/null || exit 3' ERR
 
 source $DOTFILES_DESTINATION/.bashrc
 
--- before-update.sh
+++ before-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap 'echo -e "⚠  Error ($0:$LINENO): $(sed -n "${LINENO}p" "$0" 2> /dev/null | grep -oE "\S.*\S|\S" || true)" >&2; return 3 2> /dev/null || exit 3' ERR
+trap 'echo "⚠  Error ($0:$LINENO): $BASH_COMMAND" && return 3 2> /dev/null || exit 3' ERR
 
 # rm -rf $DOTFILES_DESTINATION/.config/nvim
