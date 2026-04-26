1cd1e1e849d33338c5b3579b7fcf395dd889c08b
zdm <zdm@softvisio.net>
2026-04-26 13:28:40 +0300

    fix: fix sh error trap

--- after-update.sh
+++ after-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
-trap 'echo "⚠  Error ($0:$LINENO): $BASH_COMMAND" && return 3 2> /dev/null || exit 3' ERR
+trap 'echo "⚠  Error ($0:$LINENO, exit code: $?): $BASH_COMMAND" >&2' ERR
 
 source $DOTFILES_DESTINATION/.bashrc
 
