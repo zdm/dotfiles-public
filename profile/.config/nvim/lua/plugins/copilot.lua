52653a14b2f33578c28335c4e12aa6fcd2e2566d
zdm <zdm@softvisio.net>
2026-07-23 19:50:59 +0300

    chore: enable copilot lua

--- profile/.config/nvim/lua/plugins/copilot.lua
+++ profile/.config/nvim/lua/plugins/copilot.lua
@@ -4,6 +4,19 @@ return {
         enabled = false,
     },
     {
+        "zbirenbaum/copilot.lua",
+        dependencies = {
+            {
+                "copilotlsp-nvim/copilot-lsp",
+            },
+        },
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function ()
+            require( "copilot" ).setup( {} )
+        end
+    },
+    {
         "copilotc-nvim/copilotchat.nvim",
         dependencies = {
             {
