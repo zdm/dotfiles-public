d26ba60897c3bda1548975e596b939d6b5c0c87f
zdm <zdm@softvisio.net>
2026-08-17 14:49:21 +0300

    chore: update nvim ai

--- profile/.config/nvim/lua/plugins/ai.lua
+++ profile/.config/nvim/lua/plugins/ai.lua
@@ -1,3 +1,17 @@
+vim.api.nvim_create_user_command( "Ai", function ( opts )
+    local range = ""
+
+    if vim.api.nvim_get_mode().mode == "v" then
+        range = "'<,'>"
+    elseif opts.range > 0 then
+        range = opts.line1 .. "," .. opts.line2
+    else
+        range = "%"
+    end
+
+    vim.cmd( string.format( "%sCodeCompanion %s", range, opts.args ) )
+end, { nargs = "*", range = true } )
+
 return {
     {
         "zbirenbaum/copilot.lua",
@@ -43,25 +57,98 @@ return {
         cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionCLI", "CodeCompanionActions" },
         keys = {
             {
-                "<Leader>a",
-                mode = { "n", "i", "v" },
-                desc = "Add context to the CLI agent",
-                function ()
-                    require( "codecompanion" ).cli( "#{this}", {
-                        focus = true,
-                        prompt = true,
-                    } )
-                end,
+                "<Leader>aa",
+                "<cmd>CodeCompanion<cr>",
+                mode = { "n", "i" },
+                desc = "[Ai] Execute CodeCompanion",
             },
+            {
+                "<Leader>aa",
+                ":CodeCompanion<cr>",
+                mode = { "v" },
+                desc = "[Ai] Execute CodeCompanion",
+            },
+            {
+                "<Leader>ac",
+                "<cmd>CodeCompanionChat<cr>",
+                mode = { "n", "i", "v" },
+                desc = "[Ai] Execute CodeCompanionChat",
+            },
+            -- {
+            --     "<Leader>a",
+            --     mode = { "n", "i", "v" },
+            --     desc = "[Ai] Execute CodeCompanion CLI with current buffer",
+            --     function ()
+            --         require( "codecompanion" ).cli( "#{this}", {
+            --             focus = true,
+            --             prompt = true,
+            --         } )
+            --     end,
+            -- },
         },
         config = function ()
             require( "codecompanion" ).setup( {
+                adapters = {
+                    http = {
+                        copilot = function ()
+                            return require( "codecompanion.adapters" ).extend( "copilot", {
+                                schema = {
+                                    model = {
+                                        default = "gpt-4o", -- "gpt-4o" for FREE plan
+                                    },
+                                },
+                            } )
+                        end,
+                        ollama = function ()
+                            return require( "codecompanion.adapters" ).extend( "ollama", {
+                                env = {
+                                    url = "http://devel:11434",
+                                },
+                                schema = {
+                                    model = {
+                                        default = "qwen2.5-coder:7b", -- change to your pulled model
+                                    },
+                                    num_ctx = {
+                                        default = 16384, -- adjust context window size
+                                    },
+                                },
+                            } )
+                        end,
+                    },
+                },
                 interactions = {
-                    chat = {
-                        adapter = {
-                            name = "copilot",
-                            model = "gpt-4o", -- free plan: gpt-4o
+                    inline = {
+                        keymaps = {
+                            -- accept_change = {
+                            --     modes = { n = "ga" },
+                            --     description = "Accept the suggested change",
+                            --     opts = { nowait = true },
+                            -- },
+                            -- reject_change = {
+                            --     modes = { n = "gr" },
+                            --     description = "Reject the suggested change",
+                            --     opts = { nowait = true },
+                            -- },
                         },
+                        adapter = "copilot",
+                    },
+                    chat = {
+                        keymaps = {
+                            send = {
+                                modes = {
+                                    n = "<C-s>",
+                                    i = "<C-s>"
+                                },
+                            },
+                            close = false, -- Completely disable a default keymap
+                        },
+                        tools = {
+                            [ "insert_edit_into_file" ] = {
+                                callback = "strategies.chat.tools.insert_edit_into_file",
+                                description = "Insert edits into a file",
+                            },
+                        },
+                        adapter = "copilot",
                         opts = {
                             completion_provider = "cmp",
                         }
