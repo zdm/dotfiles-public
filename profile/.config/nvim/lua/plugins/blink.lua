f1325ca8c540cf7d08059190b201ad141c7a2a68
zdm <zdm@softvisio.net>
2026-09-16 21:36:38 +0300

    refactor: switch blink completion config to setup function

--- profile/.config/nvim/lua/plugins/blink.lua
+++ profile/.config/nvim/lua/plugins/blink.lua
@@ -29,104 +29,106 @@ return {
         build = function ()
             require( "blink.cmp" ).build():pwait()
         end,
-        opts = {
-            keymap = {
-                preset = "none",
-                [ "<C-Up>" ] = { "show", "select_prev", "fallback" },
-                [ "<C-Down>" ] = { "show", "select_next", "fallback" },
-                [ "<CR>" ] = { "accept", "fallback" },
-                [ "<Tab>" ] = { "snippet_forward", "fallback" },
-                [ "<S-Tab>" ] = { "snippet_backward", "fallback" },
-            },
-            completion = {
-                documentation = {
-                    auto_show = true,
-                },
-                ghost_text = {
-                    enabled = true,
-                },
-                list = {
-                    selection = {
-                        preselect = false,
-                        auto_insert = false,
-                    },
-                },
-            },
-            sources = {
-                default = {
-                    "snippets",
-                    "yaml_snippets",
-                    "emoji",
-                    "calc",
-                    "codecompanion",
-                    "copilot",
-                    "lsp",
-
-                    -- "buffer",
-                    -- "path",
-                },
-                providers = {
-                    snippets = {
-                        min_keyword_length = 1,
-                    },
-                    yaml_snippets = {
-                        name = "yaml_snippets",
-                        module = "yaml-snippets",
-                    },
-                    emoji = {
-                        name = "emoji",
-                        module = "blink.compat.source",
-                        transform_items = function ( ctx, items )
-                            local kind = require( "blink.cmp.types" ).CompletionItemKind.Text
-
-                            for i = 1, #items do
-                                items[ i ].kind = kind
-                            end
-
-                            return items
-                        end,
-                    },
-                    calc = {
-                        name = "calc",
-                        module = "blink.compat.source",
-                    },
-                    codecompanion = {
-                        name = "CodeCompanion",
-                        module = "codecompanion.providers.completion.blink",
-                        enabled = function ()
-                            return vim.bo.filetype == "codecompanion"
-                        end,
-                    },
-                    copilot = {
-                        name = "copilot",
-                        module = "blink-copilot",
-                        score_offset = 100,
-                        async = true,
-                    },
-                },
-            },
-            fuzzy = {
-                implementation = "rust",
-                max_typos = 0,
-                frecency = {
-                    enabled = true,
-                },
-                use_proximity = true,
-            },
-            signature = {
-                enabled = true,
-            },
-            cmdline = {
-                enabled = true,
+        config = function ()
+            require( "blink.cmp" ).setup( {
                 keymap = {
-                    preset = "inherit",
+                    preset = "none",
+                    [ "<C-Up>" ] = { "show", "select_prev", "fallback" },
+                    [ "<C-Down>" ] = { "show", "select_next", "fallback" },
+                    [ "<CR>" ] = { "accept", "fallback" },
+                    [ "<Tab>" ] = { "snippet_forward", "fallback" },
+                    [ "<S-Tab>" ] = { "snippet_backward", "fallback" },
                 },
                 completion = {
-                    menu = {
-                        auto_show = false,
+                    documentation = {
+                        auto_show = true,
+                    },
+                    ghost_text = {
+                        enabled = true,
+                    },
+                    list = {
+                        selection = {
+                            preselect = false,
+                            auto_insert = false,
+                        },
                     },
                 },
-            },
-        },
+                sources = {
+                    default = {
+                        "snippets",
+                        "yaml_snippets",
+                        "emoji",
+                        "calc",
+                        "codecompanion",
+                        "copilot",
+                        "lsp",
+
+                        -- "buffer",
+                        -- "path",
+                    },
+                    providers = {
+                        snippets = {
+                            min_keyword_length = 1,
+                        },
+                        yaml_snippets = {
+                            name = "yaml_snippets",
+                            module = "yaml-snippets",
+                        },
+                        emoji = {
+                            name = "emoji",
+                            module = "blink.compat.source",
+                            transform_items = function ( ctx, items )
+                                local kind = require( "blink.cmp.types" ).CompletionItemKind.Text
+
+                                for i = 1, #items do
+                                    items[ i ].kind = kind
+                                end
+
+                                return items
+                            end,
+                        },
+                        calc = {
+                            name = "calc",
+                            module = "blink.compat.source",
+                        },
+                        codecompanion = {
+                            name = "CodeCompanion",
+                            module = "codecompanion.providers.completion.blink",
+                            enabled = function ()
+                                return vim.bo.filetype == "codecompanion"
+                            end,
+                        },
+                        copilot = {
+                            name = "copilot",
+                            module = "blink-copilot",
+                            score_offset = 100,
+                            async = true,
+                        },
+                    },
+                },
+                fuzzy = {
+                    implementation = "rust",
+                    max_typos = 0,
+                    frecency = {
+                        enabled = true,
+                    },
+                    use_proximity = true,
+                },
+                signature = {
+                    enabled = true,
+                },
+                cmdline = {
+                    enabled = true,
+                    keymap = {
+                        preset = "inherit",
+                    },
+                    completion = {
+                        menu = {
+                            auto_show = false,
+                        },
+                    },
+                },
+            } )
+        end,
     },
 }
