0ef5752bd589778674914bcc1bc3935b13a738b8
zdm <zdm@softvisio.net>
2026-09-22 11:19:40 +0300

    chore: remove Poedit from Windows software install list

--- windows/install-software.cmd
+++ windows/install-software.cmd
@@ -6,7 +6,6 @@ call is-elevated.cmd || ( sudo -E "%~sf0" %* & exit /B )
 winget install ^
     Google.GoogleDrive ^
     TeamViewer.TeamViewer.Host ^
-    VaclavSlavik.Poedit ^
     Garmin.Express ^
     voidtools.Everything ^
     WireGuard.WireGuard ^
