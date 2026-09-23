82d4ded518a779e2f8e27982ec117fabecbe4009
zdm <zdm@softvisio.net>
2026-09-23 10:59:35 +0300

    chore: configure git hooks and SSH signing

--- windows/install-git.cmd
+++ windows/install-git.cmd
@@ -7,11 +7,17 @@ setlocal
 
 setx GIT_CONFIG_GLOBAL "%USERPROFILE%\.gitconfig"
 
-setx GIT_CONFIG_COUNT 2
+setx GIT_CONFIG_COUNT 5
 setx GIT_CONFIG_KEY_0 diffTool.guiDefault
 setx GIT_CONFIG_VALUE_0 true
 setx GIT_CONFIG_KEY_1 mergeTool.guiDefault
 setx GIT_CONFIG_VALUE_1 false
+setx GIT_CONFIG_KEY_2 core.hooksPath
+setx GIT_CONFIG_VALUE_2 %USERPROFILE%\.config\git\hooks
+setx GIT_CONFIG_KEY_3 gpg.ssh.allowedSignersFile
+setx GIT_CONFIG_VALUE_3 %USERPROFILE%\.config\git\ssh\allowed-signatures
+setx GIT_CONFIG_KEY_4 gpg.ssh.revocationFile
+setx GIT_CONFIG_VALUE_4 %USERPROFILE%\.config\git\ssh\revocatied-signatures
 
 pacman --sync --noconfirm --needed ^
     mingw-w64-x86_64-git ^
