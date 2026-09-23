eabe0006c223e25c880355c00858fcc1fd9efc31
zdm <zdm@softvisio.net>
2026-09-23 11:01:48 +0300

    fix: quote Git config paths for Windows user profile

--- windows/install-git.cmd
+++ windows/install-git.cmd
@@ -13,11 +13,11 @@ setx GIT_CONFIG_VALUE_0 true
 setx GIT_CONFIG_KEY_1 mergeTool.guiDefault
 setx GIT_CONFIG_VALUE_1 false
 setx GIT_CONFIG_KEY_2 core.hooksPath
-setx GIT_CONFIG_VALUE_2 %USERPROFILE%\.config\git\hooks
+setx GIT_CONFIG_VALUE_2 "%USERPROFILE%\.config\git\hooks"
 setx GIT_CONFIG_KEY_3 gpg.ssh.allowedSignersFile
-setx GIT_CONFIG_VALUE_3 %USERPROFILE%\.config\git\ssh\allowed-signatures
+setx GIT_CONFIG_VALUE_3 "%USERPROFILE%\.config\git\ssh\allowed-signatures"
 setx GIT_CONFIG_KEY_4 gpg.ssh.revocationFile
-setx GIT_CONFIG_VALUE_4 %USERPROFILE%\.config\git\ssh\revocatied-signatures
+setx GIT_CONFIG_VALUE_4 "%USERPROFILE%\.config\git\ssh\revocatied-signatures"
 
 pacman --sync --noconfirm --needed ^
     mingw-w64-x86_64-git ^
