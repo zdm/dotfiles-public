b271bf98426fba54e0b3179bb961120d38a4bd73
zdm <zdm@softvisio.net>
2026-09-24 11:50:58 +0300

    chore: remove disabled CodeCompanion configuration

--- profile/.config/nvim/lua/plugins/blink.lua
+++ profile/.config/nvim/lua/plugins/blink.lua
@@ -68,7 +68,6 @@ return {
                         "yaml_snippets",
                         "emoji",
                         "calc",
-                        -- "codecompanion",
                         "copilot",
                         "lsp",
 
@@ -100,13 +99,6 @@ return {
                             name = "calc",
                             module = "blink.compat.source",
                         },
-                        -- codecompanion = {
-                        --     name = "CodeCompanion",
-                        --     module = "codecompanion.providers.completion.blink",
-                        --     enabled = function ()
-                        --         return vim.bo.filetype == "codecompanion"
-                        --     end,
-                        -- },
                         copilot = {
                             name = "copilot",
                             module = "blink-copilot",
